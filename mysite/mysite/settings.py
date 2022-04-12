3d7b4b3a83c8dee33aa28d4787ae5eefa49afda0
vladi <vladilsav7772@email.com>
2022-04-12 20:03:08 +0300

    lab2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,3 +133,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+
+STATIC_URL = "/static/"
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
